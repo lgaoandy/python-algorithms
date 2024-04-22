8b5b5db3cb8acce2b40e7e2fdae79462b7d1c920
ANDY <agao@alpacasys.com>
2024-04-22 15:10:34 -0600

    came up with efficient solution

--- leetcode/3_sum.py
+++ leetcode/3_sum.py
@@ -5,6 +5,7 @@ return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, an
 Notice that the solution set must not contain duplicate triplets.
 '''
 
+# brute force method
 def three_sum(nums: list[int]) -> list[list[int]]:
     # use a dictionary to store triplets, only need the first two nums
     combinations = {}
@@ -27,6 +28,40 @@ def three_sum(nums: list[int]) -> list[list[int]]:
                             combinations[int1].append(int2)
                         triplets.append([int1, int2, int3])
     return triplets
+
+# efficent method
+def three_sum(nums: list[int]) -> list[list[int]]:
+    nums.sort()
+    result = []
+    n = len(nums)
+
+    # given one number, need to find two other numbers in the list that cancels it
+    for i in range(n - 2):
+        # solution will be same of the same value
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        
+        left = i + 1 # start on leftmost next of i
+        right = n - 1 # start on rightmost end
+        print(left, right)
+
+        while left < right:
+            total = nums[i] + nums[left] + nums[right]
+            if total < 0:
+                left += 1
+            elif total > 0:
+                right -= 1
+            else:
+                result.append([nums[i], nums[left], nums[right]])
+                # ensure the next leftmost num is not the same number
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+    return result
+
 
 if __name__ == "__main__":
     print(three_sum([-1, 0, 1, 2, -1, -4]))
