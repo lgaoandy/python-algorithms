020e162f79017a6f04349dd244974b2067401275
ANDY <andygaoofficial@gmail.com>
2024-09-09 14:53:41 -0600

    added is_subsequence problem

--- leetcode/392_is_subsequence.py
+++ leetcode/392_is_subsequence.py
@@ -9,17 +9,14 @@ class Solution():
         s_index = 0
         t_index = 0
 
-        while True:
-            if s_index > len(s):
-                return True
-            if t_index > len(t):
-                return False
+        while t_index < len(t) and s_index < len(s):
             if t[t_index] == s[s_index]:
                 s_index += 1
             t_index += 1
+        return s_index == len(s)
 
 if __name__ == "__main__":
     test = Solution()
-    print(test.is_subsequence_brute_force("abc", "ahbgdc"))
-    print(test.is_subsequence_brute_force("axc", "ahbgdc"))
-    print(test.is_subsequence_brute_force("ace", "aec"))
+    print(test.is_subsequence("abc", "ahbgdc"))
+    print(test.is_subsequence("axc", "ahbgdc"))
+    print(test.is_subsequence("ace", "aec"))
