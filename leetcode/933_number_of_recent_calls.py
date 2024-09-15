ad7901bb25614a58c481a529cb9d9b55c01527cc
andy <32784981+lgaoandy@users.noreply.github.com>
2024-09-15 11:10:18 -0600

    added number_of_recent_calls problem

---
+++ leetcode/933_number_of_recent_calls.py
@@ -0,0 +1,21 @@
+'''
+    pseudo-code:
+    - queues are first-in first-out examples
+'''
+class RecentCounter():
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        
+        while self.requests[0] < t - 3000:
+            self.requests.pop(0)
+        return len(self.requests)
+
+if __name__ == "__main__":
+    recentCounter = RecentCounter()
+    print(recentCounter.ping(1))
+    print(recentCounter.ping(100))
+    print(recentCounter.ping(3001))
+    print(recentCounter.ping(3002))
