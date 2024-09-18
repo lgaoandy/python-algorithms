025c5450e6ab964c882cd6b9b1f73d00e19da7ec
andy <32784981+lgaoandy@users.noreply.github.com>
2024-09-17 22:58:04 -0600

    improved rock_paper_scissors and guess_the_number

--- min-projects/guess_the_number/guess_the_number.py
+++ min-projects/guess_the_number/guess_the_number.py
@@ -6,6 +6,12 @@ import random
 # 2) add a handler to catch non-integer inputs and ask them to try again
 # 3) add an option to toggle hint after an incorrect guess to show whether subsequent guesses is too high or too low
 
+class Colors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
 
 def user_guess(x):
     hint = False
@@ -15,8 +21,9 @@ def user_guess(x):
 
     while True:
         attempts += 1
+        print(f"\nGuess {attempts}")
         guess = input(
-            f"Guess a number between 1 and {x}. Input a number or type \"hint\" to toggle hint: ")
+            f"Enter a number between 1 and {x} or type \"hint\" to toggle hint: ")
 
         if guess.isnumeric():
             guess = int(guess)
@@ -34,7 +41,7 @@ def user_guess(x):
                 print(f"Insane! You guessed correctly on first try! ")
             else:
                 print(
-                    f"Congratulations, you guessed correctly after {attempts} tries! ")
+                    f"{Colors.GREEN}{random_number} is correct! You got it in {attempts} tries!{Colors.END}")
             break
         else:
             if hint:
@@ -49,39 +56,44 @@ def computer_guess(x):
     low = 1
     high = x
     attempts = 0
-    print(f"It's time to let the computer guess your number!")
+    print("\nNow it's my turn to guess!")
+    input(f"Think of a number between 1 and {x}, then hit ENTER when you're ready.")
 
-    while True:
+    while low <= high:
         attempts += 1
         computer_guess = random.randint(low, high)
         result = input(
-            f"Is {computer_guess} too low(L), too high (H), or correct (C)?").lower()
+            f"\nI guess {computer_guess}\nIs it too low(L), too high(H), or correct(C)?").lower()
 
         if result == "c":
             print(f"The computer guessed correct, after {attempts} attempts!")
-            break
+            return attempts
         elif result == "l":
             low = computer_guess + 1
         elif result == "h":
             high = computer_guess - 1
-    return attempts
+    return -1
 
 
 def guess_the_number():
-    number = int(
-        input(f"Let's play a guessing game! Pick a number to set the limit: "))
+    print("\nThe computer challenges you to a guessing game!")
+    
+    limit = int(input(f"Pick a number to set the limit: "))
 
-    user_attempts = user_guess(number)
-    comp_attempts = computer_guess(number)
+    user_attempts = user_guess(limit)
+    comp_attempts = computer_guess(limit)
 
-    if user_attempts < comp_attempts:
+    if comp_attempts == -1:
         print(
-            f"Congrats! You won against the computer with {user_attempts} attempts")
+            f"\n{Colors.RED}You cheated! GAME OVER!{Colors.END}")
+    elif user_attempts < comp_attempts:
+        print(
+            f"\n{Colors.GREEN}Congrats! You won against me with {user_attempts} attempts!{Colors.END}")
     elif user_attempts > comp_attempts:
         print(
-            f"You lose. The computer has beat you with {comp_attempts} attempts")
+            f"\n{Colors.RED}You lose! I have beat you with {comp_attempts} attempts!{Colors.END}")
     else:
-        print(f"You and the computer tied both with {user_attempts} attempts!")
+        print(f"\n{Colors.YELLOW}You and me have tied with {user_attempts} attempts!{Colors.END}")
 
 
 guess_the_number()
--- min-projects/rock_paper_scissors/rock_paper_scissors.py
+++ min-projects/rock_paper_scissors/rock_paper_scissors.py
@@ -1,6 +1,12 @@
 import random
 import math
 
+class Colors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
 
 def get_computer_choice():
     return random.choice(["rock", "paper", "scissors"])
@@ -20,25 +26,28 @@ def get_player_choice():
                 return "scissors"
         else:
             print(
-                f"...{choice} is not a valid choice. Please try again...")
+                f"\"{choice}\" is not a valid choice. Please try again...\n")
             continue
 
 
 def get_rounds():
     while True:
-        rounds = input(f"Choose number of rounds: ")
+        rounds = input(f"\nDecide the number of rounds to play (1-10): ")
 
-        if rounds.isnumeric():
+        if rounds.isnumeric() and int(rounds) > 0 and int(rounds) <= 10:
+            print(f"Best out of {rounds} rounds. Let the games begin!")
             return int(rounds)
         else:
-            print(f"Must choose a number!")
+            print(f"\nPlease choose a number between 1 and 10!")
+
+
+def print_points(score):
+    return "point" if score == 1 else "points"
 
 
 def rock_paper_scissors():
-    # 1 - rock
-    # 2 - paper
-    # 3 - scissors
-
+    print("\nThe computer challenges you to a duel of Rock, Paper, Scissors!\nA game of pure wits and skill passed down from generations.")
+    
     rounds = get_rounds()
     winning_score = math.floor(rounds/2) + 1
 
@@ -49,12 +58,14 @@ def rock_paper_scissors():
     player_score = 0
 
     while computer_score < winning_score and player_score < winning_score and computer_score + player_score < rounds:
+        print(f"\nRound {computer_score + player_score + 1}")
+        
         while True:
             computer_choice = get_computer_choice()
             player_choice = get_player_choice()
 
             if computer_choice == player_choice:
-                print(f"TIED! Go again...")
+                print(f"{Colors.YELLOW}TIED{Colors.END}, the computer chose the same... Go again!\n")
                 continue
             else:
                 break
@@ -64,21 +75,23 @@ def rock_paper_scissors():
                 player_choice == "scissors" and computer_choice == "rock"):
             computer_score += 1
             print(
-                f"Computer chose {computer_choice}, which beats {player_choice}, COMPUTER scores 1 point!")
+                f"The computer chose {computer_choice}, which beats {player_choice}, {Colors.RED}COMPUTER scores 1 point!{Colors.END}")
         else:
             player_score += 1
             print(
-                f"Computer chose {computer_choice}, which is weak against {player_choice}, YOU score 1 point!")
+                f"The computer chose {computer_choice}, which is weak against {player_choice}, {Colors.GREEN}YOU score 1 point!{Colors.END}")
 
+    score_unit = print_points(player_score)
+    
     if computer_score == player_score:
         print(
-            f"You and computer tied with {player_score} points. Try an odd number of rounds next time.")
+            f"\n{Colors.YELLOW}You and computer tied with {player_score} {score_unit}{Colors.END}. Try an odd number of rounds next time.")
     elif computer_score < player_score:
         print(
-            f"You win with {player_score} points versus {computer_score} from the computer!")
+            f"\n{Colors.GREEN}Congratulations! You win with {player_score} {score_unit}{Colors.END} versus {computer_score} from the computer!")
     else:
         print(
-            f"You lost with {player_score} points versus {computer_score} from the computer...")
+            f"\n{Colors.RED}Unfortunately, you lost with {player_score} {score_unit}{Colors.END} versus {computer_score} from the computer...")
 
 
 rock_paper_scissors()
