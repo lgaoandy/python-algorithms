e335befd86d1778d2fdf9f69da10836738b2a564
ANDY <andygaoofficial@gmail.com>
2024-09-18 18:09:24 -0600

    adjusted mad libs project

---
+++ projects/mad_libs/README.md
@@ -0,0 +1,5 @@
+# Mad Libs
+
+This is a terminal program that intakes a file and writes a mad libs for you replacing every word covered in <> brackets
+
+## How to use
--- projects/mad_libs/mad_libs.py
+++ projects/mad_libs/mad_libs.py
@@ -1,16 +1,25 @@
-import re
 import typing
+import string
 
 class Colors:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
+    CYAN = '\033[96m'
     END = '\033[0m'
 
 class MadLib:
-    def load_file(self, filename: str) -> typing.TextIO:
-        return open(filename, 'r')
-    
+    def __init__(self) -> None:
+        self.special_prompts = {}
+
+    def load_file(self, filename: str) -> list[str]:
+        content = []
+        file_content = open(filename, 'r')
+
+        for line in file_content:
+            content.append(line.strip())
+        return content
+
 
     def check_a_or_an(self, noun: str) -> str:
         vowels = "aeiou"
@@ -19,54 +28,77 @@ class MadLib:
         else:
             return "a"
         
-    
-    def split_prompts(self, content: typing.TextIO) -> tuple[list[str], list[str]]:
-        stripped_content = []
-        prompts = []
-        for line in content:
-            line = line.strip()
-            
-            if len(line) > 0:
-                prompt = []
-                stripped_line = [line[0]]
-                for i in range(1, len(line)):
-                    if stripped_line[-1] != "<":
-                        stripped_line.append(line[i])
-                    elif line[i] == ">":
-                        stripped_line.append(line[i])
-                        prompts.append("".join(prompt))
-                        prompt = []
-                    else:
-                        prompt.append(line[i])
-                stripped_content.append("".join(stripped_line).split())
-        return prompts, stripped_content
+
+    def check_special_prompt(self, s: str) -> tuple[bool, str]:
+        prompt = []
+        is_special = False
+        for i in s:
+            if i.isdigit():
+                is_special = True
+            else:
+                prompt.append(i)
+        return (is_special, "".join(prompt))
+
+
+    def has_prompt(self, s: str) -> bool:
+        return "<" in s and ">" in s and s.index("<") < s.index(">")
     
 
-    def get_user_prompts(self, prompts: list[str]) -> list[str]:
-        n = len(prompts)
-        substitutions = []
-        for i in range(n):
-            determiner = self.check_a_or_an(prompts[i])
-            i = input(f"\n{Colors.GREEN}Enter {determiner} {prompts[i]}{Colors.END} ({i} of {n}): ")
-            substitutions.append(i)
-        return substitutions
-    
+    def add_special_prompt(self, prompt: str, answer: str) -> None:
+        self.special_prompts[prompt] = answer
+
+
+    def ask_user(self, prompt: str) -> str:
+        answer = ""
+        is_special_prompt, s_prompt = self.check_special_prompt(prompt)
+
+        if is_special_prompt:
+            if prompt not in self.special_prompts.keys():
+                determiner = self.check_a_or_an(s_prompt)
+                answer = input(f"\n{Colors.GREEN}Enter {determiner} {s_prompt}{Colors.END}: ")
+                self.add_special_prompt(prompt, answer)
+            else:
+                answer = self.special_prompts[prompt]
+        else:
+            determiner = self.check_a_or_an(s_prompt)
+            answer = input(f"\n{Colors.GREEN}Enter {determiner} {s_prompt}{Colors.END}: ")
+        return answer
+
 
     def play(self, filename: str) -> None:
         content = self.load_file(filename)
-        prompts, stripped_content = self.split_prompts(content)
-        print(stripped_content)
-        substitutions = self.get_user_prompts(prompts)
+        
+        for k in range(len(content)):
+            sentence = content[k].strip()
+            
+            if len(sentence) > 0:
+                completed = [sentence[0]]
+                prompt_list = []
 
-        # substitute in content
-        for i in range(len(stripped_content)):
-            for j in range(len(stripped_content[i])):
-                if "<>" in stripped_content[i][j]:
-                    stripped_content[i][j] = stripped_content[i][j].replace("<>", substitutions.pop())
-            line = " ".join(stripped_content[i]) 
-            print(line)
+                for i in range(1, len(sentence)):
+                    if completed[-1] != "<":
+                        completed.append(sentence[i])
+                    elif sentence[i] == ">":
+                        completed.pop()
+                        prompt = "".join(prompt_list)
+                        prompt_list = []
+                        answer = self.ask_user(prompt)     
+                        completed.append(answer)
+                    else:
+                        prompt_list.append(sentence[i])
+                content[k] = "".join(completed)
+            else:
+                content[k] = sentence
+            k += 1
+        
+        print("\n=================================")
+        print(f"\n{Colors.CYAN}{content[0]}{Colors.END}")
+
+        for i in range(1, len(content)):
+            print(content[i])
+            
 
 
 if __name__ == "__main__":
     madlibs = MadLib()
-    madlibs.play("projects/mad_libs/mad_libs2.txt")
+    madlibs.play("projects/mad_libs/mad_libs1.txt")
