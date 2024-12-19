48ded38d9388ffdd808618957a261b8a2832eaec
ANDY <andygaoofficial@gmail.com>
2024-12-19 12:38:36 -0700

    completed maximum depth of binary tree problem

--- leetcode/104_maximum_depth_of_binary_tree.py
+++ leetcode/104_maximum_depth_of_binary_tree.py
@@ -6,28 +6,38 @@ from datatypes.binary_tree import TreeNode
 class Solution:
     '''
         constriants:
-        - 
-
-        potential questions to ask interviewer
-        - 
+        - -100 <= node.val <= 100
+        - number of nodes in the tree is in the range[0, 10^4]
 
         pseudo-code
-        - 
+        - traverse the nodes of the tree recursively
 
         analysis
-        - 
+        - solution checks every node which O(n)
     '''
     def max_depth(self, root: Optional[TreeNode]) -> int:
-        pass
+        # base case
+        if not isinstance(root, TreeNode): # checks whether root is a tree node
+            return 0
+        elif root.val is None: # if current node is empty, depth is zero
+            return 0
+        else:
+            return 1 + max(self.max_depth(root.left), self.max_depth(root.right))
     
+
 
 if __name__ == "__main__":
     # build binary tree of example
-    tree = TreeNode()
-    tree.build([3,9,20,None,None,15,7])
-    tree.display()
+    tree1 = TreeNode()
+    tree1.build([3,9,20,None,None,15,7])
+    tree1.display()
+
+    tree2 = TreeNode()
+    tree2.build([1,None,2])
+    tree2.display()
 
     # build solution
     s = Solution()
-    print(s.sum_power_of_two(3))
+    print(s.max_depth(tree1))
+    print(s.max_depth(tree2))
 
