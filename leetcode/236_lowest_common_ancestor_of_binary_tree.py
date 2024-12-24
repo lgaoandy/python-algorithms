a01900845e561145b2dcff30cd3c93b7ed4f336d
ANDY <andygaoofficial@gmail.com>
2024-12-24 11:14:06 -0700

    added optimized solution

--- leetcode/236_lowest_common_ancestor_of_binary_tree.py
+++ leetcode/236_lowest_common_ancestor_of_binary_tree.py
@@ -18,12 +18,13 @@ class Solution:
         - returning a queue of values, in which we compare, using LIFA to find the LCM
 
         analysis
-        - 
+        - time complexity: O(n), however, it is 2 DFS, and a lot in between steps
+        - average complexity is a lot higher than just n
     '''
-    def lowest_common_ancestor_number(self, root: TreeNode, p: int, q: int) -> int:
+    def lowest_common_ancestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
 
         # find a node of target value, then return the path of values 
-        def dfs(node: Optional[TreeNode], path: list[int], target: int) -> list[int]:
+        def path_dfs(node: Optional[TreeNode], path: list[int], target: int) -> list[int]:
             if not node: 
                 return []
             
@@ -35,25 +36,60 @@ class Solution:
             if node.val == target:
                 return new_path
             
-            left = dfs(node.left, new_path, target)
-            right = dfs(node.right, new_path, target)
+            left = path_dfs(node.left, new_path, target)
+            right = path_dfs(node.right, new_path, target)
             
             if len(left) > 0:
                 return left
             else:
                 return right
-    
-        p_ancestors = dfs(root, [], p)
-        q_ancestors = dfs(root, [], q)
+        
 
-        p_ancestors.reverse()
-        q_ancestors = set(q_ancestors)
+        def lca(list1: list[int], list2: list[int]) -> int:
+            list1.reverse()
+            list2 = set(list2)
+            for num in list1:
+                if num in list2:
+                    return num
+                
+        
+        def find_dfs(node: Optional[TreeNode], target: int) -> TreeNode:
+            if not node:
+                return 
+            elif node.val == target:
+                return node
+            
+            left = find_dfs(node.left, target)
+            right = find_dfs(node.right, target)
+            
+            if isinstance(left, TreeNode):
+                return left
+            return right
 
-        # find the first number that also exists in q_ancestor
-        for num in p_ancestors:
-            if num in q_ancestors:
-                return num
+                
+        p_ancestors = path_dfs(root, [], p.val)
+        q_ancestors = path_dfs(root, [], q.val)
+        return find_dfs(root, lca(p_ancestors, q_ancestors))
 
+
+    '''
+        solution:
+        - time complexity: O(n)
+        - average time complexity is less n
+        - takes full advantage of the assumption that p, q always exists in the tree and so there will always be an answer
+        - in this approach, as soon as one of values (p or q) is found, it is returned
+        - when a deep node is matched, it is taken back to traverse through the parent until a parent node is found
+    '''
+    def lowest_common_ancestor_optimized(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        if not root or root.val == p.val or root.val == q.val:
+            return root
+        
+        l = self.lowest_common_ancestor(root.left, p, q)
+        r = self.lowest_common_ancestor(root.right, p, q)
+
+        if l and r:
+            return root
+        return l or r
 
 
 if __name__ == "__main__":
@@ -62,8 +98,14 @@ if __name__ == "__main__":
     # build binary tree
     tree1 = construct_tree([3,5,1,6,2,0,8,None,None,7,4])
     tree1.display()
-    print(s.lowest_common_ancestor(tree1, 5, 1))
+    solution1 = s.lowest_common_ancestor_optimized(tree1, construct_tree([5]), construct_tree([1]))
+
+    if isinstance(solution1, TreeNode):
+        solution1.display()
 
     tree2 = construct_tree([3,5,1,6,2,0,8,None,None,7,4])
     tree2.display()
-    print(s.lowest_common_ancestor(tree2, 5, 4))
+    solution2 = s.lowest_common_ancestor_optimized(tree2, construct_tree([6]), construct_tree([4]))
+
+    if isinstance(solution2, TreeNode):
+        solution2.display()
