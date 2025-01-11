1eaf61d76b4dd363f3ad786b13e041373151ea5a
ANDY <andygaoofficial@gmail.com>
2025-01-10 17:53:04 -0700

    added leetcode 75 - letters combination of a phone number

--- leetcode/17_letters_combinations_of_a_phone_number.py
+++ leetcode/17_letters_combinations_of_a_phone_number.py
@@ -1,31 +1,27 @@
-# class Solution:
-#     '''
-#         Constriants:
-#         - 
+class Solution:
+    '''
+        Learning sample
+    '''
+    def letterCombinations(self, digits: str) -> list[str]:
+        res = []
+        telephone = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
 
-#         Comments/questions for interviewer
-#         - 
+        def backtrack(i, current):
+            if len(current) == len(digits):
+                res.append(current)
+                return
+            
+            for c in telephone[digits[i]]:
+                backtrack(i + 1, current + c)
 
-#         Pseudo-code
-#         - 
-
-#         Analysis
-#         - 
-#     '''
-#     def letterCombinations(self, digits: str) -> list[str]:
-#         telephone = {
-#             2: "abc", 3: "def", 4: "ghi", 5: "jkl",
-#             6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"
-#         }
-
-#         def combine(digits, combinations):
-#             for digit in digits:
-#                 for letters in telephone[digit]:
-#                     for combination in combinations:
-#                         combination.push(digit)
-
+        if digits:
+            backtrack(0, "")
+        return res
             
 
-# if __name__ == "__main__":
-#     s = Solution()
-#     print(s.letterCombinations("23"))
+if __name__ == "__main__":
+    s = Solution()
+    print(s.letterCombinations("23"))
