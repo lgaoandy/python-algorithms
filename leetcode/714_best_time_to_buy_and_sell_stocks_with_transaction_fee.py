b4d499d5996ac069a525254e003944b6ef730a8e
ANDY <andygaoofficial@gmail.com>
2025-01-22 12:33:38 -0700

    added leetcode 75 - best time to buy and sell stocks with transaction fee

---
+++ leetcode/714_best_time_to_buy_and_sell_stocks_with_transaction_fee.py
@@ -0,0 +1,45 @@
+class Solution:
+    '''
+        Intuition
+        - We only need to find the maximum profit, meaning we do not need to know how to get there
+        - Transactional fee sets the min difference you can profit from a buy and sell
+
+        Dynamic Programming Approach
+        - 
+    '''
+    def max_profit_with_grid(self, prices: list[int], fee: int) -> int:
+        n = len(prices)
+        profits = [[0] * n] * n
+        max_profits = [0] * n
+        
+        for i in range(0, n):
+            for j in range(i+1, n):
+                current_sell = prices[j] - prices[i] - fee
+
+                # append max of the previous value
+                if i >= 2 and j >= 2:
+                    current_sell += max_profits[i-1]
+
+                profits[i][j] = max(0, current_sell, profits[i][j-1])
+                max_profits[j] = max(max_profits[j], profits[i][j])
+        return max_profits[-1]
+    
+
+    def max_profit(self, prices: list[int], fee: int) -> int:
+        pos = -prices[0]
+        profit = 0
+
+        for i in range(1, len(prices)):
+            print(pos, profit)
+            pos = max(pos, profit - prices[i])
+            profit = max(profit, pos + prices[i] - fee)
+        return profit
+
+
+if __name__ == "__main__":
+    s = Solution()
+    # print(s.max_profit([1,3,2,8,4,9], 2))
+    # print(s.max_profit([1,3,7,5,10,3], 3))
+    # print(s.max_profit([1,3,2,8,4,9], 2))
+    # print(s.max_profit([1,4,6,2,8,3,10,14], 3))
+    print(s.max_profit([1,2,1,5,3,5,5,4,1,5], 0))
