48ea5aeedf7fc0d247cfc7ccdb55eef212b58b2b
ANDY <andygaoofficial@gmail.com>
2025-01-28 16:19:50 -0700

    added leetcode 75 - added implement trie

--- leetcode/trie/208_implement_trie.py
+++ leetcode/trie/208_implement_trie.py
@@ -1,23 +1,46 @@
 class Trie:
     def __init__(self):
-        self.children = set()
-        pass
+        self.children = {}
+        self.isWord = False
 
     def insert(self, word: str) -> None:
-        pass
+        curr = self
+        n = len(word)
+
+        for i in range(n):
+            letter = word[i]
+            if letter not in curr.children.keys():
+                curr.children[letter] = Trie()
+            curr = curr.children[letter]
+        curr.isWord = True
+        
 
     def search(self, word: str) -> bool:
-        pass
+        curr = self
+        
+        for letter in word:
+            if letter not in curr.children.keys():
+                return False
+            curr = curr.children[letter]
+        return curr.isWord
+    
 
     def startsWith(self, prefix: str) -> bool:
-        pass
+        curr = self
+
+        for letter in prefix:
+            if letter not in curr.children.keys():
+                return False
+            curr = curr.children[letter]
+        return True
+
 
 if __name__ == "__main__":
 
     trie = Trie()
-    trie.insert("apple")
-    trie.search("apple")    # return True
-    trie.search("app")      # return False
-    trie.startsWith("app")  # return True
-    trie.insert("app")
-    trie.search("app")      # return True
+    print(trie.insert("apple"))
+    print(trie.search("apple"))    # return True
+    print(trie.search("app"))      # return False
+    print(trie.startsWith("app"))  # return True
+    print(trie.insert("app"))
+    print(trie.search("app"))      # return True
