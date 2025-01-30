fc93750e2610198470b050071de993b4a429b5df
ANDY <andygaoofficial@gmail.com>
2025-01-30 14:31:29 -0700

    added neetcodeio solution to 2493

--- leetcode/2493_divide_nodes_into_the_max_num_of_groups.py
+++ leetcode/2493_divide_nodes_into_the_max_num_of_groups.py
@@ -1,3 +1,5 @@
+from collections import defaultdict, deque
+
 class Solution:
     '''
         Intuition
@@ -22,7 +24,62 @@ class Solution:
             
         return adjacency
 
+    
+    def magnificentSetsNeetcode(self, n: int, edges: list[list[int]]) -> int:
+        adj = defaultdict(list)
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+
+        def get_connected_component(src):
+            q = deque([src])
+            component = set([src])
+            while q:
+                node = q.popleft()
+                for nei in adj[node]:
+                    if nei in component:
+                        continue
+                    q.append(nei)
+                    component.add(nei)
+                    visit.add(nei)
+            return component
+        
+
+        def longest_path(src):
+            q = deque([(src, 1)]) # (node, group)
+            dist = { src: 1 } # node -> length from src + 1
+
+            while q:
+                node, length = q.popleft()
+                for nei in adj[node]:
+                    if nei in dist:
+                        if dist[nei] == length:
+                            return -1
+                        continue
+                    q.append((nei, length + 1))
+                    dist[nei] = length + 1
+            return max(dist.values())
+
+        visit = set()
+        res = 0
+        for i in range(1, n + 1):
+            if i in visit:
+                continue
+            
+            visit.add(i)
+            component = get_connected_component(i)
+
+            max_cnt = 0
+            for src in component:
+                length = longest_path(src)
+                if length == -1:
+                    return -1
+                max_cnt = max(max_cnt, length)
+            res += max_cnt
+        return res
+
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.magnificentSets(6, [[1,2],[1,4],[1,5],[2,6],[2,3],[4,6]]))
+    print(s.magnificentSetsNeetcode(6, [[1,2],[1,4],[1,5],[2,6],[2,3],[4,6]]))
