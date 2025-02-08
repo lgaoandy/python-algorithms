8cdccb6fa68ae2b2a0ec5ad45d1109b20a66a0a6
andy <32784981+lgaoandy@users.noreply.github.com>
2025-02-08 11:41:33 -0700

    added leetcode daily - design a number container system - attempt 2 - linked list

--- leetcode/2349_design_a_num_container_system.py
+++ leetcode/2349_design_a_num_container_system.py
@@ -1,3 +1,5 @@
+from heapq import heapify, heappush
+
 '''
     Intuition:
     - The challenge involves in optimizing solution for the array spacing
@@ -6,26 +8,89 @@
     - Alternatively, we cannot use a single dictionary due to the find() function
     
     Approach:
-    - Initialize the class with an array AND a dictionary
-    - change() will fill the array with a array of a tuple containing both the index and 
+    - Use a linked list to store numbers within a unfilled sequence
+    - Use a dictionary to track the index to the node of the linked list
+    - Use a min heap to sequence from smallest to highest index
+    - When change() is called: [O(logn) time]
+        - we can use a binary search on the heap to find the index before the desired index
+        - we can use the dictionary to direct us to the correct node in the linked list
+        - then we can add the node into the linked list in index order
+    - When find() is called: [O(n) time]
+        - we can search by traversing the linked list until the number is found
 '''
+class Node:
+    def __init__(self, val, index):
+        self.val = val
+        self.index = index
+        self.next = None
+        
+    
+    def __str__(self):
+        values = []
+        node = self
+        while node:
+            values.append(f"({node.index}, {node.val})")
+            node = node.next
+        return "\n" + " > ".join(values)
+    
+
 class NumberContainers:
     def __init__(self):
-        self.nums = []
-
-
-    def change(self, index: int, number: int) -> None:
-        if index >= len(self.nums):
-            return        
-        self.nums[index] = number
+        self.nodes = None
+        self.index_to_nodes = {}
+        self.indexes = []
+        heapify(self.indexes)
         
 
-    def find(self, number: int) -> int:
-        for i in range(len(self.nums)):
-            if self.nums[i] == number:
-                return i
-        return -1
+    def index_before(self, index: int):
+        r = len(self.indexes)
+        
+        if r == 0:
+            return 0
+        l = 0
+        while r - l != 1:
+            m = (l + r) // 2
+            if self.indexes[m] < index:
+                l = m
+            else:
+                r = m
+                
+        if self.indexes[l] > index:
+            return 0
+        return self.indexes[l]
+        
+        
+    def change(self, index: int, number: int) -> None:
+        # check if index exists
+        if index in self.index_to_nodes: # replace value
+            node = self.index_to_nodes[index]
+            node.val = number
+        
+        else: # new value
+            i = self.index_before(index)
+            new_node = Node(number, index)
+            
+            if i == 0: # replaces head
+                new_node.next = self.nodes
+                self.nodes = new_node
+                
+            else: # insert node
+                node = self.index_to_nodes[i]
+                new_node.next = node.next
+                node.next = new_node
+                
+            self.index_to_nodes[index] = new_node
+            heappush(self.indexes, index)
+                
 
+    def find(self, number: int) -> int:
+        node = self.nodes
+        while node:
+            if node.val == number:
+                return node.index
+            node = node.next
+        return -1
+                
 
 if __name__ == "__main__":
     s = NumberContainers()
