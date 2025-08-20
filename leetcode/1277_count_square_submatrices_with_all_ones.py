237b6e75ad39a2a1dd82af5efaff2734714e4e60
andy <32784981+lgaoandy@users.noreply.github.com>
2025-08-20 00:29:15 -0600

    added 1277 - unsolved

---
+++ leetcode/1277_count_square_submatrices_with_all_ones.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        
+
+if __name__ == "__main__":
+    s = Solution()
+    
+    print(s.countSquares([
+        [0,1,1,1],
+        [1,1,1,1],
+        [0,1,1,1]
+    ]))
+    
+    print(s.countSquares([
+        [1,0,1],
+        [1,1,0],
+        [1,1,0]
+    ]))
