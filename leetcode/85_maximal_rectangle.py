309e3974256c7de6cf543aaca2ce3e4e4a0ac848
andy <32784981+lgaoandy@users.noreply.github.com>
2025-08-26 21:32:26 -0600

    progress on maximal rectangle

--- leetcode/85_maximal_rectangle.py
+++ leetcode/85_maximal_rectangle.py
@@ -1,47 +1,24 @@
 class Solution:
+    '''
+        Histogram Solution:
+    '''
     def maximalRectangle(self, matrix: list[list[str]]) -> int:
         m, n = len(matrix), len(matrix[0])
+        max_area = 0
+        sums = [[0] * n for _ in range(m)]
         
-        # setup rows and columns dp
-        rows = [[0] * n for _ in range(m)]
-        cols = [[0] * n for _ in range(m)]
+        # Populate first row
+        for j in range(m):
+            sums[0][j] = int(matrix[0][j])
         
-        # iterate through matrix, populating rows
-        for i in range(m):
-            rows[i][n-1] = int(matrix[i][n-1])
-            for j in range(n-2, -1, -1):
-                if matrix[i][j] == "1":
-                    rows[i][j] = rows[i][j+1] + 1
-        
-        # iterate through matrix, populating cols
-        for j in range(n):
-            cols[m-1][j] = int(matrix[m-1][j])
-            for i in range(m-2, -1, -1): 
-                if matrix[i][j] == "1":
-                    cols[i][j] = 1 + cols[i+1][j]
-        
-        for i in range(m):
-            print(rows[i])
-            
-        print()
-        for i in range(m):
-            print(cols[i])
-        
-        area = [[0] * n for _ in range(m)]
-        # iterate through grid, calculating areas
-        for i in range(m):
+        for i in range(1, m):
             for j in range(n):
-                ii = rows[i][j]
-                jj = cols[i][j]
-                if ii <= 1 or jj <= 1:
-                    area[i][j] = ii * jj
-                else:
-                    area1 = max()
-                    
+                if sums[i-1][j] > 0 or 
+                sums[i][j] = int(matrix[i][j]) + sums[i-1][j]
+                
+            
         
-        print()
-        for i in range(m):
-            print(area[i])
+        
                 
     
 if __name__ == "__main__":
